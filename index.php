71f9391bd12594752d4d9be4d7abd981bda5a2b3
olivorel <olivorel45@hotmail.fr>
2017-10-05 18:34:21 +0200

    modification des fichiers index + aurelie.css

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 // Connection DB
 
-// Param
-$db = "mysql:host=localhost;dbname=AuPenDuick;charset=utf8";
-$user = "root";
-$password = "mysql01"; // Remplacer par votre password
+//Param
+//$db = "mysql:host=localhost;dbname=AuPenDuick;charset=utf8";
+//$user = "root";
+//$password = "mysql01"; // Remplacer par votre password
 
-include "function/connect.php";
-$bdd = connect($db, $user, $password);
+//include "function/connect.php";
+//$bdd = connect($db, $user, $password);
 
 ?>
 
@@ -19,9 +19,9 @@ $bdd = connect($db, $user, $password);
 
     <title>My little library</title>
 
-    <link rel="stylesheet" type="text/css" media="all" href="pierre.css" />
-    <link rel="stylesheet" type="text/css" media="all" href="fabrice.css" />
-    <link rel="stylesheet" type="text/css" media="all" href="aurelie.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="css/pierre.css"/>
+    <link rel="stylesheet" type="text/css" media="all" href="css/fabrice.css"/>
+    <link rel="stylesheet" type="text/css" media="all" href="css/aurelie.css"/>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -33,3 +33,52 @@ include "aurelie.php";
 include "footer.php";
 ?>
 
+<body>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="media">
+            <div class="col-md-6">
+                <div class="media-left ">
+                    <img class="media-object img-responsive" src="pictures/avatar2.png" alt="movies">
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="media-body media-middle">
+                    <h4 class="media-heading">Films</h4>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                        ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row music_zone">
+        <div class="media">
+            <div class="col-md-6">
+                <div class="media-body media-middle text_music">
+                    <h4 class="media-heading">Music</h4>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                        ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="media-right ">
+                    <img class="media-object img-responsive" src="pictures/casque2.png" alt="movies">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
