f56402d21d0959d3bd36e8e661a65ab608e7e17b
Kypli <pierre.amboise@yahoo.fr>
2017-10-05 23:30:27 +0200

    Divers  + bdd

--- pierre.php
+++ pierre.php
@@ -1,6 +1,5 @@
 <?php
-Update("UPDATE Users SET pseudo = :pseudo, password = :password", array(":id" => $_SESSION['id'], ":zone_x_destination" => $_POST['deplacement_carte_x'], ":zone_y_destination" => $_POST['deplacement_carte_y']));
-
+update("UPDATE Users SET pseudo = :pseudo, password = :password WHERE UsersId = :UsersId", array(":pseudo" => "pierre", ":password" => "aaa", ":UsersId" => 1));
 
 
 
