47a89f9eefc50aab7bd120b5e810b7240683400e
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-10-06 13:25:39 +0200

    Rajout BDD

--- db/MyLittleLibrary.sql
+++ db/MyLittleLibrary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  jeu. 05 oct. 2017 à 23:29
+-- Généré le :  ven. 06 oct. 2017 à 13:24
 -- Version du serveur :  5.7.19-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
@@ -58,11 +58,18 @@ CREATE TABLE `Consumn` (
 --
 
 CREATE TABLE `Favorite` (
-  `ConsumnId` int(11) NOT NULL,
+  `FavoriteId` int(11) NOT NULL,
   `User` int(11) NOT NULL,
-  `Library` int(11) NOT NULL,
-  `LibraryProduct` int(11) NOT NULL
+  `Library` int(11) DEFAULT NULL,
+  `LibraryProduct` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `Favorite`
+--
+
+INSERT INTO `Favorite` (`FavoriteId`, `User`, `Library`, `LibraryProduct`) VALUES
+(13, 1, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -91,10 +98,24 @@ INSERT INTO `Library` (`LibraryId`, `name`) VALUES
 
 CREATE TABLE `LibraryMovies` (
   `LibraryMoviesId` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `genre` int(11) NOT NULL,
-  `duration` time NOT NULL
+  `description` text NOT NULL,
+  `genre` int(11) DEFAULT NULL,
+  `duration` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `LibraryMovies`
+--
+
+INSERT INTO `LibraryMovies` (`LibraryMoviesId`, `user`, `name`, `description`, `genre`, `duration`) VALUES
+(1, 1, 'Scrubs', 'J.D. est un jeune médecin qui débute sa carrière dans l\'hôpital du Sacré-Coeur. Il vit avec son meilleur ami Turk', 3, '01:00:00'),
+(2, 1, 'Doctor House', 'C\'est un génie mais un salaud', 1, '01:00:00'),
+(3, 1, 'Prison Break', 'Michael Scofield s\'engage dans une véritable lutte contre la montre : son frère Lincoln est dans le couloir de la mort, en attente de son exécution. Persuadé de son innocence mais à court de solutions, Michael décide de se faire incarcérer à son tour dans le pénitencier d\'état de Fox River pour organiser leur évasion...', NULL, NULL),
+(4, 2, 'Stargate SG-1', 'Un anneau de trois mètres de diamètre fait d’un métal inconnu sur Terre constitue en fait une porte ouvrant un passage vers d’autres planètes. Des équipes de militaires et de scientifiques explorent ces différentes planètes, cherchant de nouvelles technologies pour combattre notamment les Goa’ulds, une espèce extraterrestre qui parasite des humains. Parmi ces équipes d’exploration, SG-1 devient la plus réputée, avec le colonel O’Neill, l’égyptologue Jackson, le major Carter et Teal’c, un Jaffa venant de Chulak, un autre monde.', NULL, NULL),
+(5, 1, 'How I Met Your Mother', 'Ted se remémore ses jeunes années, lorsqu\'il était encore célibataire. Il revoit avec nostalgie ses moments d\'égarements et de troubles, ses rencontres et ses recherches effrénées du Grand Amour.', NULL, NULL),
+(6, 1, 'Weeds', 'Dans une banlieue respectable de Los Angeles, les apparences sont parfois trompeuses. Les sales petits secrets des uns et des autres vont peu à peu se faire jour. Nancy Botwin, une mère célibataire, vend de la marijuana depuis la mort subite de son mari. C\'est sa façon à elle de subvenir aux besoins de sa famille...', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -114,7 +135,7 @@ CREATE TABLE `LibraryMoviesGenre` (
 INSERT INTO `LibraryMoviesGenre` (`LibraryMoviesGenreId`, `genre`) VALUES
 (1, 'comedy'),
 (2, 'horror'),
-(3, 'famliy'),
+(3, 'family'),
 (4, 'thriller'),
 (5, 'fantasy'),
 (6, 'fiction'),
@@ -222,7 +243,8 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`UsersId`, `Pseudo`, `Password`) VALUES
-(1, 'pierre', 'aaa');
+(1, 'pierre', 'aaa'),
+(2, 'aurelie', 'coucou');
 
 --
 -- Index pour les tables déchargées
@@ -248,7 +270,7 @@ ALTER TABLE `Consumn`
 -- Index pour la table `Favorite`
 --
 ALTER TABLE `Favorite`
-  ADD PRIMARY KEY (`ConsumnId`),
+  ADD PRIMARY KEY (`FavoriteId`),
   ADD KEY `User` (`User`),
   ADD KEY `Movie` (`Library`);
 
@@ -325,7 +347,7 @@ ALTER TABLE `Consumn`
 -- AUTO_INCREMENT pour la table `Favorite`
 --
 ALTER TABLE `Favorite`
-  MODIFY `ConsumnId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `FavoriteId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `Library`
@@ -337,7 +359,7 @@ ALTER TABLE `Library`
 -- AUTO_INCREMENT pour la table `LibraryMovies`
 --
 ALTER TABLE `LibraryMovies`
-  MODIFY `LibraryMoviesId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `LibraryMoviesId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `LibraryMoviesGenre`
@@ -373,7 +395,7 @@ ALTER TABLE `Order_`
 -- AUTO_INCREMENT pour la table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `UsersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `UsersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
